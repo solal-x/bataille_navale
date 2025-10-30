3da1c61366e7a1947e5c8356f269d1da99723218
Solal XAVIER <solal.xavier@hotmail.fr>
2025-10-30 11:11:32 +0100

    V1.3
    ajout bateau

---
+++ bateau.py
@@ -0,0 +1,6 @@
+class Bateau:
+    def __init__ (self, ligne, colonne, longueur=1, vertical=False):
+        self.ligne = ligne
+        self.colonne = colonne
+        self.longueur = longueur
+        self.vertical = vertical
